85dcbeaea822e267897734c2dede83cb5250838f
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-06 13:15:31 +0530

    Reverse Integer

---
+++ ReverseInteger.java
@@ -0,0 +1,22 @@
+public class ReverseInteger {
+    public static void main(String[] args){
+        System.out.println(new ReverseInteger().reverse(1534236469));
+    }
+    public int reverse(int x) {
+        long num=0;
+        while(x!=0){
+            int d = x%10;
+            if(x<0){
+                num=num*10-d;
+            }
+            else{
+                num=num*10+d;
+            }
+            x=x/10;
+        }
+        if(num>Integer.MAX_VALUE || num<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)num;
+    }
+}
