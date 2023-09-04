a8e03d9ebae76039e9261572889e770d115eaa15
Rajdeep <rajdeepmukherjee2002@gmail.com>
2023-09-04 22:53:38 +0530

    .

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,10 @@
 class TwoSum {
     public static void main(String[] args) {
-        
+        int[] arr={};
+        System.out.println(fun(arr));
+    }
+
+    private static char[] fun(int[] arr) {
+        return null;
     }
 }
